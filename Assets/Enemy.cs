b593377795ed3a1cf93e5d4dbbbb93ec43f67495
Schilperoort,Lucas L.W <l.schilperoort@student.fontys.nl>
2019-01-25 19:53:44 +0100

    inital game commit

---
+++ Assets/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+    public abstract class Enemy:MonoBehaviour, IResettable
+    {
+    Vector3 startPosition;
+    Quaternion startRotation;
+
+
+
+    void IResettable.Reset()
+    {
+        transform.position = startPosition;
+        transform.rotation = startRotation;
+    }
+}
---
+++ Assets/SnailMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnailMovement : MonoBehaviour {
+
+    [SerializeField]
+    float speed;
+
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(new Vector3(0, -(speed * Time.deltaTime), 0));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * 60 * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.back * 60 * Time.deltaTime);
+        }
+	}
+}
